aa7826cca0752ee213ee8e0798e81a37f3bc5310
Brian Ketelsen <bketelsen@gmail.com>
2021-03-10 08:13:25 -0500

    initial draft

---
+++ template/config/plugins.js
@@ -0,0 +1,22 @@
+module.exports = ({ env }) => ({
+    upload: {
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      },
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    }
+  }
+)
